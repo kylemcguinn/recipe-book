5636b391eb254920f2b021614d8cf3b08f1a9155
Kyle McGuinn <kmcguinn@caravanhealth.com>
2024-07-02 15:44:23 -0400

    Added carousel for recipes

--- recipebook.web/src/components/HomePage/HomePage.vue
+++ recipebook.web/src/components/HomePage/HomePage.vue
@@ -4,6 +4,11 @@ import { onMounted, ref } from 'vue'
 import RecipeCardTemplate from '../RecipesPage/RecipesCard.vue'
 import RecipesAddCard from '../RecipesPage/RecipesAddCard.vue';
 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const recipes = ref<RecipeCard[]>([]);
 
 onMounted(() => {
@@ -24,12 +29,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-wrap justify-center">
-    <div class="ml-2 mb-2"><RecipesAddCard/></div>
-    <div v-for="recipe in recipes" :key="recipe.id" class="ml-2 mb-2">
-      <RecipeCardTemplate :recipe="recipe"></RecipeCardTemplate>
+  <div>
+    <div class="float-left mr-12">
+      <RecipesAddCard />
     </div>
+    <swiper :slidesPerView="'auto'" :spaceBetween="30" :loop="false" :navigation="true" :mousewheel="true" :keyboard="{
+      enabled: true,
+    }" :modules="[Navigation, Keyboard, Mousewheel]" class="mySwiper">
+      <swiper-slide v-for="recipe in recipes" :key="recipe.id" class="w-52">
+        <RecipeCardTemplate :recipe="recipe"></RecipeCardTemplate>
+      </swiper-slide>
+
+    </swiper>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.swiper-slide {
+  width: 13rem;
+}
+</style>
